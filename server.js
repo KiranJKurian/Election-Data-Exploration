eb34d9d0e0db8d44b7cb76606e6e24d243d8bf27
Neil Malu <Neil@nwk-13-209.nwk.ruw.rutgers.edu>
2016-12-01 13:44:05 -0500

    Set up the back end. Need to test db connection

---
+++ server.js
@@ -0,0 +1,41 @@
+var http = require('http');
+var express = require('express');
+var mysql = require('mysql');
+
+var app = express();
+app.use('/assets', express.static('assets'));
+
+/*******DB CONNECTION *******/
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'mydb',     
+});
+
+connection.connect(function(err){
+	if(err){
+		console.log("Error connecting to db");
+		return;
+	}
+	else
+		console.log("Connected to db");
+});
+
+connection.end();
+/*******ROUTES*******/
+app.get('/', function(req,res){
+	res.redirect('/home');
+});
+
+app.get('/home', function(req, res){
+	res.sendFile(__dirname + "/assets/views/home.html");
+});
+
+app.get('/bundle.js', function(req, res){
+	res.sendFile(__dirname + "/assets/js/bundle.js")
+});
+
+
+/*******START SERVER*******/
+app.listen(8080 || process.env.port);
